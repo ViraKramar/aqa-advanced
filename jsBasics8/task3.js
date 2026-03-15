6ac6d346146161740c71918409c7e0a69074b94f
Vira Kramar <kramar5red@gmail.com>
2026-03-15 19:51:08 +0200

    Add jsBasics8

---
+++ jsBasics8/task3.js
@@ -0,0 +1,24 @@
+async function fetchTodo() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  if (!res.ok) throw new Error("Failed to fetch todo");
+  return res.json();
+}
+
+async function fetchUser() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  if (!res.ok) throw new Error("Failed to fetch user");
+  return res.json();
+}
+
+(async () => {
+  try {
+    const [todo, user] = await Promise.all([fetchTodo(), fetchUser()]);
+    console.log("Promise.all todo:", todo);
+    console.log("Promise.all user:", user);
+
+    const firstResult = await Promise.race([fetchTodo(), fetchUser()]);
+    console.log("Promise.race first result:", firstResult);
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+})();
